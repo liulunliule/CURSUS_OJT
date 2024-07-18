47ccb4e836151645a125a8f2c26cceb8eadc92b5
eltammie <lennminh@gmail.com>
2024-07-18 13:50:40 +0700

    live streams API

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import savedCourseReducer from "./features/savedCourseSlice";
+import liveStreamsReducer from './features/liveStreamsSlice';
+import chatContentReducer from './features/chatContentSlice';
 
 export const store = configureStore({
     reducer: {
         savedCourse: savedCourseReducer,
+        liveStreams: liveStreamsReducer,
+        chatContent: chatContentReducer,
     },
 });
