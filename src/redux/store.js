0011a13a9716694d34d8d1b9c61279ab2f3c2b5b
eltammie <lennminh@gmail.com>
2024-07-19 08:42:52 +0700

    instructor notifications API

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import savedCourseReducer from "./features/savedCourseSlice";
 import liveStreamsReducer from './features/liveStreamsSlice';
 import chatContentReducer from './features/chatContentSlice';
+import instructorNotificationsReducer from './features/instructorNotificationsSlice';
 
 export const store = configureStore({
     reducer: {
         savedCourse: savedCourseReducer,
         liveStreams: liveStreamsReducer,
         chatContent: chatContentReducer,
+        instructorNotifications: instructorNotificationsReducer,
     },
 });
