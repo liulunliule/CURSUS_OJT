f26a041db772049b9dc92115f87351f9b3213212
nguyenquocdatoriginals <quocdat87vt@gmail.com>
2024-07-29 11:02:01 +0700

    update snapshot for BlogDetailView

--- src/pages/BlogDetailView/BlogDetailView.test.js
+++ src/pages/BlogDetailView/BlogDetailView.test.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { BrowserRouter as Router } from 'react-router-dom';
-import BlogDetailView from './index';
-import { BlogDetailImage } from '../../assets';
+import { MemoryRouter } from 'react-router-dom';
+import BlogDetailView from '.';
 
-describe('BlogDetailView', () => {
-  test('renders BlogDetailView correctly', () => {
-    render(
-      <Router>
-        <BlogDetailView />
-      </Router>
-    );
-
-    expect(screen.getByText('Blog Title Here')).toBeInTheDocument();
-
-    const backToBlogLink = screen.getByText('Back to Blog');
-    expect(backToBlogLink).toBeInTheDocument();
-    expect(backToBlogLink).toHaveAttribute('href', '/secondLayout/blog');
-
-    const blogImage = screen.getByAltText('Blog detail');
-    expect(blogImage).toBeInTheDocument();
-    expect(blogImage).toHaveAttribute('src', BlogDetailImage);
-
-    expect(screen.getByText('109k views')).toBeInTheDocument();
-    expect(screen.getByText('March 10, 2020')).toBeInTheDocument();
-
-    expect(screen.getByText('« Lorem ipsum dolor sit amet, consectetur adipiscing elit.')).toBeInTheDocument();
-    expect(screen.getByText('Vestibulum vulputate nulla quis dignissim ultricies. »')).toBeInTheDocument();
-  });
+test('renders Blog Detail View component correctly', () => {
+  const { asFragment } = render(
+    <MemoryRouter>
+      <BlogDetailView />
+    </MemoryRouter>
+  );
+  expect(asFragment()).toMatchSnapshot();
 });
