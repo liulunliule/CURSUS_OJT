aa500c133b7abb1008a84d31aacfabc34abd2aa5
NguyenThiNga111 <nguyentronghuu.07052003@gmail.com>
2024-07-29 17:34:34 +0700

    update code snapshot

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,6 @@ import '@testing-library/jest-dom/extend-expect';
 import fetchMock from 'jest-fetch-mock';
 
 fetchMock.enableMocks();
-// Mô phỏng matchMedia
 window.matchMedia = window.matchMedia || (() => ({
     matches: false,
     addListener: () => {},
