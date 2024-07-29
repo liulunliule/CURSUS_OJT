cb2b8d3fdf6b9ef9a8fcbbfd229130dac99692a6
nguyenquocdatoriginals <quocdat87vt@gmail.com>
2024-07-29 10:52:33 +0700

    update snapshot for ComingSoon

--- src/pages/ComingSoon/ComingSoon.test.js
+++ src/pages/ComingSoon/ComingSoon.test.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import ComingSoon from './index';
+import { render } from '@testing-library/react';
+import ComingSoon from '.';
 
-describe('ComingSoon', () => {
-  it('renders ComingSoon component', () => {
-    const { container } = render(<ComingSoon />);
-    expect(container).toBeDefined();
-  });
-
-  it('shows error message when email is invalid', async () => {
-    render(<ComingSoon />);
-    const emailInput = screen.getByPlaceholderText(/Email address/i);
-    const notifyButton = screen.getByRole('button', { name: /Notify Me/i });
-    fireEvent.change(emailInput, { target: { value: 'invalid-email' } });
-    fireEvent.submit(notifyButton);
-  });
-
-  it('submits the form with a valid email', async () => {
-    render(<ComingSoon />);
-    const emailInput = screen.getByPlaceholderText(/Email address/i);
-    const notifyButton = screen.getByRole('button', { name: /Notify Me/i });
-    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-    fireEvent.submit(notifyButton);
-  });
+test('renders Coming Soon component correctly', () => {
+  const { asFragment } = render(<ComingSoon />);
+  expect(asFragment()).toMatchSnapshot();
 });
