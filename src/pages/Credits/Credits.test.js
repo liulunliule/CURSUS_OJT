dba16d87849ada71950309238525065d7a378d44
nguyenquocdatoriginals <quocdat87vt@gmail.com>
2024-07-30 08:38:06 +0700

    update snapshot for Credits

--- src/pages/Credits/Credits.test.js
+++ src/pages/Credits/Credits.test.js
@@ -1,34 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
-import { useFetchCreditsData } from './data';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import { store } from '../../redux/store';
+import Credits from '.';
 
-jest.mock('./data', () => ({
-  useFetchCreditsData: jest.fn(),
-}));
-
-describe('Credits Component', () => {
-  const mockData = {
-    addedCredits: { amount: '$100', method: 'Credit Card', description: 'purchases' },
-    transactions: [
-      { amount: '$100', method: 'Credit Card', date: '2023-07-27' },
-      { amount: '$50', method: 'PayPal', date: '2023-07-28' },
-    ],
-    status: 'succeeded',
-    error: null,
-  };
-
-  it('renders loading state', () => {
-    useFetchCreditsData.mockReturnValue({ status: 'loading' });
-  });
-
-  it('renders error state', () => {
-    useFetchCreditsData.mockReturnValue({ ...mockData, status: 'failed', error: 'Failed to fetch data' });
-  });
-
-  it('renders credits data correctly', () => {
-    useFetchCreditsData.mockReturnValue(mockData);
-  });
-
-  it('applies active class when isShowAll is true', () => {
-    useFetchCreditsData.mockReturnValue(mockData);
-  });
+test('test Credits', () => {
+  const tree = renderer.create(
+    <Provider store={store}>
+        <MemoryRouter>
+            <Credits/>
+        </MemoryRouter>
+    </Provider>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
