abd2f48dd772fbfdd72d8edbb6d54283aa0f97f6
nguyenquocdatoriginals <quocdat87vt@gmail.com>
2024-07-30 08:29:19 +0700

    update snapshot for OurBlog, CompanyDetails, Press

--- src/pages/Press/Press.test.js
+++ src/pages/Press/Press.test.js
@@ -1,62 +1,18 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
-import Press from './index';
-import { useFetchPressData } from './data';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import { store } from '../../redux/store';
+import Press from '.';
 
-jest.mock('./data', () => ({
-  useFetchPressData: jest.fn(),
-}));
-
-describe('Press Component', () => {
-  const mockData = {
-    news: [
-      { Press_News_ID: 1, Press_At: '2023-07-27', Title: 'News Title 1', Intro: 'News Intro 1' },
-      { Press_News_ID: 2, Press_At: '2023-07-28', Title: 'News Title 2', Intro: 'News Intro 2' },
-    ],
-    releases: [
-      { Press_Releases_ID: 1, Press_At: '2023-07-27', Title: 'Release Title 1' },
-      { Press_Releases_ID: 2, Press_At: '2023-07-28', Title: 'Release Title 2' },
-    ],
-    status: 'succeeded',
-    error: null,
-  };
-
-  it('renders loading state', () => {
-    useFetchPressData.mockReturnValue({ ...mockData, status: 'loading' });
-
-    render(<Press />);
-    expect(screen.getByText('Loading...')).toBeInTheDocument();
-  });
-
-  it('renders error state', () => {
-    useFetchPressData.mockReturnValue({ ...mockData, status: 'failed', error: 'Failed to fetch data' });
-
-    render(<Press />);
-    expect(screen.getByText('Error: Failed to fetch data')).toBeInTheDocument();
-  });
-
-  it('renders press data correctly', () => {
-    useFetchPressData.mockReturnValue(mockData);
-
-    render(<Press />);
-
-    mockData.news.forEach((news) => {
-      expect(screen.getByText(news.Title)).toBeInTheDocument();
-      expect(screen.getByText(news.Intro)).toBeInTheDocument();
-    });
-
-    mockData.releases.forEach((release) => {
-      expect(screen.getByText(release.Title)).toBeInTheDocument();
-    });
-  });
-
-  it('handles navigation correctly', () => {
-    useFetchPressData.mockReturnValue(mockData);
-
-    render(<Press />);
-    const aboutNavItem = screen.getByText('About');
-    fireEvent.click(aboutNavItem);
-    expect(aboutNavItem).toHaveClass('selected');
-  });
+test('test Press', () => {
+  const tree = renderer.create(
+    <Provider store={store}>
+        <MemoryRouter>
+            <Press/>
+        </MemoryRouter>
+    </Provider>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
